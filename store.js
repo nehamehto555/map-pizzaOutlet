0ceb54f8784ebedde1df5e05be911150347be3c7
Neha Mehto <nehamehto555@gmail.com>
2024-09-12 23:16:51 +0530

    add pizza outlet file

---
+++ store.js
@@ -0,0 +1,256 @@
+// this is geoJSON formate for encoding a variety of geographic data structures//   
+ //{
+//   "type": "Feature",
+//   "geometry": {
+//     "type": "Point",
+//     "coordinates": [125.6, 10.1]
+//   },
+//   "properties": {
+//     "name": "Dinagat Islands"
+//   }
+// }
+  
+  const storeList = [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [88.363892, 22.572645] // Kolkata, West Bengal
+          },
+          "properties": {
+            "name": "Pizza outlet 1",
+            "address": "Park Street, Kolkata, West Bengal, 700016, India",
+            "phone": "+91 9123456789"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [76.267303, 9.931233] // Kochi, Kerala
+          },
+          "properties": {
+            "name": "Pizza outlet 2",
+            "address": "MG Road, Ernakulam, Kochi, Kerala, 682016, India",
+            "phone": "+91 8989891234"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [80.946159, 26.846694] // Lucknow, Uttar Pradesh
+          },
+          "properties": {
+            "name": "Pizza outlet 3",
+            "address": "Hazratganj, Lucknow, Uttar Pradesh, 226001, India",
+            "phone": "+91 8123456789"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [76.779417, 30.733315] // Chandigarh
+          },
+          "properties": {
+            "name": "Pizza outlet 4",
+            "address": "Sector 17, Chandigarh, 160017, India",
+            "phone": "+91 9876543210"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.474442, 17.385044] // Hyderabad, Telangana
+          },
+          "properties": {
+            "name": "Pizza outlet 5",
+            "address": "Banjara Hills, Hyderabad, Telangana, 500034, India",
+            "phone": "+91 7654321890"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [70.783316, 22.303894] // Rajkot, Gujarat
+          },
+          "properties": {
+            "name": "Pizza outlet 6",
+            "address": "Dr. Yagnik Road, Rajkot, Gujarat, 360001, India",
+            "phone": "+91 8765432109"
+          }
+        },
+      
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [19.083646, 82.028131]
+      },
+      "properties": {
+        "name": "Pizza outlet 7",
+        "address": "Jagdalpur, Jagdalpur Tahsil, Bastar, Chhattisgarh, 494001, India",
+        "phone": "+91 8989897632"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [31.103558, 77.1712891]
+      },
+      "properties": {
+        "name": "Pizza outlet 8",
+        "address": "Ripon Hospital Road, Sniwoan, Shimla, Himachal Pradesh, 171001, India",
+        "phone": "+91 9808975466"
+      }
+    },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [78.486671, 17.385044] // Hyderabad, Telangana
+          },
+          "properties": {
+            "name": "Pizza outlet 9",
+            "address": "Begumpet, Hyderabad, Telangana, 500016, India",
+            "phone": "+91 9988776655"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [73.847784, 18.520430] // Pune, Maharashtra
+          },
+          "properties": {
+            "name": "Pizza outlet 10",
+            "address": "MG Road, Camp, Pune, Maharashtra, 411001, India",
+            "phone": "+91 9123456789"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [77.603287, 12.971599] // Bangalore, Karnataka
+          },
+          "properties": {
+            "name": "Pizza outlet 11",
+            "address": "Brigade Road, Bangalore, Karnataka, 560001, India",
+            "phone": "+91 9876543210"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [80.270718, 13.082680] // Chennai, Tamil Nadu
+          },
+          "properties": {
+            "name": "Pizza outlet 12",
+            "address": "T Nagar, Chennai, Tamil Nadu, 600017, India",
+            "phone": "+91 1234567890"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [88.363895, 22.572646] // Kolkata, West Bengal
+          },
+          "properties": {
+            "name": "Pizza outlet 13",
+            "address": "Park Street, Kolkata, West Bengal, 700016, India",
+            "phone": "+91 9090909090"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [76.936637, 8.524139] // Thiruvananthapuram, Kerala
+          },
+          "properties": {
+            "name": "Pizza outlet 14",
+            "address": "MG Road, Thiruvananthapuram, Kerala, 695001, India",
+            "phone": "+91 9876512345"
+          }
+        },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [72.5713621, 23.022505] // Ahmedabad, Gujarat
+              },
+              "properties": {
+                "name": "Pizza outlet 15",
+                "address": "CG Road, Navrangpura, Ahmedabad, Gujarat, 380009, India",
+                "phone": "+91 9876567890"
+              }
+            },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [74.358748, 31.549720] // Amritsar, Punjab
+              },
+              "properties": {
+                "name": "Pizza outlet 16",
+                "address": "Lawrence Road, Amritsar, Punjab, 143001, India",
+                "phone": "+91 9123467890"
+              }
+            },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [75.8577258, 26.912434] // Jaipur, Rajasthan
+              },
+              "properties": {
+                "name": "Pizza outlet 17",
+                "address": "MI Road, Jaipur, Rajasthan, 302001, India",
+                "phone": "+91 9012345678"
+              }
+            },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [77.739029, 28.984461] // Meerut, Uttar Pradesh
+              },
+              "properties": {
+                "name": "Pizza outlet 18",
+                "address": "Abu Lane, Meerut, Uttar Pradesh, 250001, India",
+                "phone": "+91 9834567810"
+              }
+            },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [73.786246, 15.299326] // Panaji, Goa
+              },
+              "properties": {
+                "name": "Pizza outlet 19",
+                "address": "MG Road, Panaji, Goa, 403001, India",
+                "phone": "+91 8899887766"
+              }
+            },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": [92.717636, 11.623377] // Port Blair, Andaman & Nicobar Islands
+              },
+              "properties": {
+                "name": "Pizza outlet 21",
+                "address": "Aberdeen Bazar, Port Blair, Andaman & Nicobar Islands, 744101, India",
+                "phone": "+91 9845678901"
+              }
+            }
+          ];
+      
